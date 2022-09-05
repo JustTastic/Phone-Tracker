c3c04847ab0526e3bdc77b07d9161db1a33128a8
Abdirahman <74493469+JustTastic@users.noreply.github.com>
2022-09-05 16:54:10 +0300

    third commit

--- main.py
+++ main.py
@@ -4,28 +4,36 @@ from opencage.geocoder import OpenCageGeocode
 from phonenumbers import geocoder
 import folium
 
-pepnumber = phonenumbers.parse(mynumber)
-location = geocoder.description_for_number(pepnumber, 'en')
+def main():
+    afterHyphenNumber = mynumber.strip("-")
+    pepnumber = phonenumbers.parse(afterHyphenNumber.strip(" "))
+    location = geocoder.description_for_number(pepnumber, 'en')
 
-print(location)
+    print(location)
 
-from phonenumbers import carrier
+    from phonenumbers import carrier
 
-service_pro = phonenumbers.parse(mynumber)
-print(carrier.name_for_number(service_pro, 'en'))
+    service_pro = phonenumbers.parse(mynumber)
+    print(carrier.name_for_number(service_pro, 'en'))
 
-key = 'a15216a390094978a45359244788cd21'
-geocoder = OpenCageGeocode(key)
-query = str(location)
-result = geocoder.geocode(query)
-# print(result)
+    key = 'a15216a390094978a45359244788cd21'
+    Geocoder = OpenCageGeocode(key)
+    query = str(location)
+    result = Geocoder.geocode(query)
+    # print(result)
 
-lat = result[0] ['geometry']['lat']
-lng = result[0] ['geometry']['lng']
-# print(lat, lng)
+    lat = result[0] ['geometry']['lat']
+    lng = result[0] ['geometry']['lng']
+    # print(lat, lng)
 
-myMap = folium.Map(location=[lat, lng], zoom_start= 9)
-folium.Marker([lat,lng], popup=location).add_to(myMap)
+    myMap = folium.Map(location=[lat, lng], zoom_start= 9)
+    folium.Marker([lat,lng], popup=location).add_to(myMap)
 
-myMap.save("location.html")
-print("DONE")
+    myMap.save("location.html")
+    print("DONE")
+
+
+if __name__ == '__main__':
+    main()    
+
+
